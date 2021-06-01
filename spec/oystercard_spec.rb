1662291c2f3336f5544abb5ad79941c04dd0ae7b
Ashley Slaney <ashleyslaney96@gmail.com>
2021-06-01 16:14:30 +0100

    #5: added test for top up method and passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it 'has a starting balance' do
     expect(subject.balance).to eq(0)
+  end
+
+  context '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'allows the user to top up their card' do
+      subject.top_up(20)
+      expect(subject.balance).to eq(20)
+    end
   end
 end
