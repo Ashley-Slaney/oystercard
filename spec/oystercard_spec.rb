aa2863865fb408d471a033449c224c518326dad8
Ashley Slaney <ashleyslaney96@gmail.com>
2021-06-02 15:25:58 +0100

    #6: maximum balance constant and error message in top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,5 +14,16 @@ describe Oystercard do
       subject.top_up(20)
       expect(subject.balance).to eq(20)
     end
+
+    it 'has a max limit of £90' do
+      subject.top_up(30)
+      expect { subject.top_up(61) }.to raise_error("The maximum limit is £#{Oystercard::MAX_LIMIT}")
+    end
+
+    it 'does not fail with £90 top up' do
+      subject.top_up(40)
+      subject.top_up(50)
+      expect(subject.balance).to eq(90)
+    end
   end
 end
